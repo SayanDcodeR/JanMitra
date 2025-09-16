c2c4b49119da2145fcb0fd5235a1f52b3e12d206
Ayan <duttaayan070@gmail.com>
2025-09-16 13:34:57 +0530

    Added changes

--- app.js
+++ app.js
@@ -5,12 +5,22 @@ const MONGO_URL = "mongodb://127.0.0.1:27017/communityOne";
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const ExpressError = require("./utils/ExpressError.js");
+// const ExpressError = require("./utils/ExpressError.js");
 const session=require("express-session");
 const flash=require("connect-flash");
-app.get("/",(req,res)=>{
-    res.send("Hello");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("/home",(req,res)=>{
+    res.render("home.ejs");
 });
+app.post("/report",(req,res)=>{
+    console.log(req.body.issue);
+})
 app.listen("8000",()=>{
     console.log("Server connected");
 });
